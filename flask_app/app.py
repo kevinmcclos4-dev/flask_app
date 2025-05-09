90760894b1519b598f9aa983a459faa7afaefa6b
Kevin McCloskey <kevinmccloskey@Kevins-MacBook-Pro.local>
2025-05-09 12:09:07 -0400

    Test automating Jenkins via Github webhooks

--- flask_app/app.py
+++ flask_app/app.py
@@ -22,8 +22,7 @@ def api_data():
         'data': [
             {'id': 1, 'name': 'Item 1'},
             {'id': 2, 'name': 'Item 2'},
-            {'id': 3, 'name': 'Item 3'},
-            {'id': 4, 'name': 'Item 4'}
+            {'id': 3, 'name': 'Item 3'}
         ]
     })
 
